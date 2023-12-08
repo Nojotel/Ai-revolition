7b0a0d165f9345c80c8c7d096dc6a0deecc3e64e
Nojotel <nojotel@yandex.ru>
2023-12-08 20:00:28 +0300

    /
    /

--- components/screens/authForms/RegFormPage.tsx
+++ components/screens/authForms/RegFormPage.tsx
@@ -1,29 +1,24 @@
-import { SocialButtons } from '@/components/common'
-import { RegisterForm } from '@/components/forms'
-import Link from 'next/link'
+import { SocialButtons } from "@/components/common";
+import { RegisterForm } from "@/components/forms";
+import Link from "next/link";
 
 export default function RegFormPage() {
-	return (
-		<div className='flex min-h-full flex-1 flex-col justify-center mx-14'>
-			<div className=''>
-				<h2 className='mt-9 text-left text-3xl font-semibold leading-9 tracking-tight text-textlight'>
-					Регистрация
-				</h2>
-			</div>
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center mx-14">
+      <div className="">
+        <h2 className="mt-9 text-left text-3xl font-semibold leading-9 tracking-tight text-textlight">Регистрация</h2>
+      </div>
 
-			<div className='mt-3 mx-auto'>
-				<RegisterForm />
-				<SocialButtons />
-				<p className='my-6 text-center text-sm text-textlight'>
-					Есть аккаунт?{' '}
-					<Link
-						href='/auth/login'
-						className=' text-textlight hover:text-texthover'
-					>
-						Войти
-					</Link>
-				</p>
-			</div>
-		</div>
-	)
+      <div className="mt-3 mx-auto">
+        <RegisterForm />
+        <SocialButtons />
+        <p className="my-6 text-center text-sm text-textlight">
+          Есть аккаунт?{" "}
+          <Link href="/auth/login" className=" text-textlight hover:text-texthover">
+            Войти
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
