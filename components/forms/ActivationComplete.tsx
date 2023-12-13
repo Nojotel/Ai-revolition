1aaebb522c4a17731eaa4eebf85c4c5ec747ef3b
Nojotel <nojotel@yandex.ru>
2023-12-13 23:32:08 +0300

    Update ActivationComplete.tsx

--- components/forms/ActivationComplete.tsx
+++ components/forms/ActivationComplete.tsx
@@ -1,36 +1,35 @@
-'use client'
+"use client";
 
-import { Form } from '@/components/forms'
-import { useActivationComplete } from '@/hooks'
-import { useSearchParams } from 'next/navigation'
+import { Form } from "@/components/forms";
+import { useActivationComplete } from "@/hooks";
+import { useSearchParams } from "next/navigation";
 
 export default function ActivationCompleteForm() {
-	const searchParams = useSearchParams()
-	const initialEmail = String(searchParams.get('email'))
-	const { email, isLoading, onChange, onSubmit } =
-		useActivationComplete(initialEmail)
+  const searchParams = useSearchParams();
+  const initialEmail = String(searchParams.get("email"));
+  const { email, isLoading, onChange, onSubmit } = useActivationComplete(initialEmail);
 
-	interface Config {
-		labelText: string
-		labelId: string
-		type: string
-		value: string
-		link?: {
-			linkText: string
-			linkUrl: string
-		}
-		required?: boolean
-	}
+  interface Config {
+    labelText: string;
+    labelId: string;
+    type: string;
+    value: string;
+    link?: {
+      linkText: string;
+      linkUrl: string;
+    };
+    required?: boolean;
+  }
 
-	const config: Config[] = []
+  const config: Config[] = [
+    {
+      labelText: "Email",
+      labelId: "email",
+      type: "text",
+      value: email,
+      required: true,
+    },
+  ];
 
-	return (
-		<Form
-			config={config}
-			isLoading={isLoading}
-			btnText='Отправить повторно'
-			onChange={onChange}
-			onSubmit={onSubmit}
-		/>
-	)
+  return <Form config={config} isLoading={isLoading} btnText="Отправить повторно" onChange={onChange} onSubmit={onSubmit} isButtonActive={isButtonActive} />;
 }
