474209e8175f60442acb539a8a7f450cad788f12
Nojotel <nojotel@yandex.ru>
2023-12-13 22:42:42 +0300

    Update index.ts

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -2,6 +2,6 @@ export { default as ActivationCompleteForm } from "./ActivationComplete";
 export { default as Form } from "./Form";
 export { default as Input } from "./Input";
 export { default as LoginForm } from "./LoginForm";
-//export { default as PasswordResetConfirmForm } from "./PasswordResetConfirmForm";
+export { default as PasswordResetConfirmForm } from "./PasswordResetConfirmForm";
 export { default as PasswordResetForm } from "./PasswordResetForm";
 export { default as RegisterForm } from "./RegisterForm";
